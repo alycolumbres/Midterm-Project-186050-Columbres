7862221fdd70b785e7bbb43a3f107c2f3588804a
Alysha Columbres <alysha.columbres@obf.ateneo.edu>
2020-03-16 17:24:17 +0800

    Made the functional test file

---
+++ midtermproject_functional_tests.py
@@ -0,0 +1,111 @@
+from selenium import webdriver
+import unittest
+
+
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+
+	def tearDown(self):
+		self.browser.quit()
+
+	def test_can_display_ingredients_list(self):
+		self.browser.get('http://localhost:8000/ingredients_list')
+		self.assertIn('Ingredients - List', self.browser.title)
+		self.assertEqual(
+			'http://localhost:8000/ingredients_list',
+			self.browser.getCurrentUrl()
+		)
+
+	def test_can_display_ingredients_detail(self):
+		self.browser.get('http://localhost:8000/ingredients_detail')
+		self.assertIn('Ingredients - Detail', self.browser.title)
+		self.assertEqual(
+			'http://localhost:8000/ingredients_detail',
+			self.browser.getCurrentUrl()
+		)
+
+	def test_can_display_ingredients_update(self):
+		self.browser.get('http://localhost:8000/ingredients_update')
+		self.assertIn('Ingredients - Update', self.browser.title)
+		self.assertEqual(
+			'http://localhost:8000/ingredients_update',
+			self.browser.getCurrentUrl()
+		)
+
+	def test_can_display_ingredients_create(self):
+		self.browser.get('http://localhost:8000/ingredients_create')
+		self.assertIn('Ingredients - Create', self.browser.title)
+		self.assertEqual(
+			'http://localhost:8000/ingredients_create',
+			self.browser.getCurrentUrl()
+		)
+
+	def test_can_display_recipes_list(self):
+		self.browser.get('http://localhost:8000/recipes_list')
+		self.assertIn('Recipes - List', self.browser.title)
+		self.assertEqual(
+			'http://localhost:8000/recipes_list',
+			self.browser.getCurrentUrl()
+		)
+
+	def test_can_display_recipes_detail(self):
+		self.browser.get('http://localhost:8000/recipes_detail')
+		self.assertIn('Recipes - Detail', self.browser.title)
+		self.assertEqual(
+			'http://localhost:8000/recipes_detail',
+			self.browser.getCurrentUrl()
+		)
+
+	def test_can_display_recipes_update(self):
+		self.browser.get('http://localhost:8000/recipes_update')
+		self.assertIn('Recipes - Update', self.browser.title)
+		self.assertEqual(
+			'http://localhost:8000/recipes_update',
+			self.browser.getCurrentUrl()
+		)
+
+	def test_can_display_recipes_create(self):
+		self.browser.get('http://localhost:8000/recipes_create')
+		self.assertIn('Ingredients - Create', self.browser.title)
+		self.assertEqual(
+			'http://localhost:8000/recipes_create',
+			self.browser.getCurrentUrl()
+		)
+
+	def test_can_display_orders_list(self):
+		self.browser.get('http://localhost:8000/orders_list')
+		self.assertIn('Orders - List', self.browser.title)
+		self.assertEqual(
+			'http://localhost:8000/orders_list',
+			self.browser.getCurrentUrl()
+		)
+
+	def test_can_display_orders_detail(self):
+		self.browser.get('http://localhost:8000/orders_detail')
+		self.assertIn('Orders - Detail', self.browser.title)
+		self.assertEqual(
+			'http://localhost:8000/orders_detail',
+			self.browser.getCurrentUrl()
+		)
+
+	def test_can_display_orders_update(self):
+		self.browser.get('http://localhost:8000/orders_update')
+		self.assertIn('Orders - Update', self.browser.title)
+		self.assertEqual(
+			'http://localhost:8000/orders_update',
+			self.browser.getCurrentUrl()
+		)
+
+	def test_can_display_orders_create(self):
+		self.browser.get('http://localhost:8000/orders_create')
+		self.assertIn('Orders - Create', self.browser.title)
+		self.assertEqual(
+			'http://localhost:8000/orders_create',
+			self.browser.getCurrentUrl()
+		)
+
+	self.fail('Finish the test!')
+
+if __name__ == '__main__':
+	unittest.main(warnings = 'ignore')
