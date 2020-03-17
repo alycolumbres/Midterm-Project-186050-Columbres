8e26f4d45ec30b9f6ab77c7f9351bbcd40082e7c
Alysha Columbres <alysha.columbres@obf.ateneo.edu>
2020-03-17 16:35:58 +0800

    Fixed my functional_tests.py file

--- midtermproject_functional_tests.py
+++ midtermproject_functional_tests.py
@@ -1,5 +1,6 @@
-from selenium import webdriver
 import unittest
+
+from selenium import webdriver
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -12,99 +13,62 @@ class NewVisitorTest(unittest.TestCase):
 	def test_can_display_ingredients_list(self):
 		self.browser.get('http://localhost:8000/ingredients_list')
 		self.assertIn('Ingredients - List', self.browser.title)
-		self.assertEqual(
-			'http://localhost:8000/ingredients_list',
-			self.browser.getCurrentUrl()
-		)
+		self.assertIn('http://localhost:8000/ingredients_list', self.browser.current_url)
 
 	def test_can_display_ingredients_detail(self):
 		self.browser.get('http://localhost:8000/ingredients_detail')
 		self.assertIn('Ingredients - Detail', self.browser.title)
-		self.assertEqual(
-			'http://localhost:8000/ingredients_detail',
-			self.browser.getCurrentUrl()
-		)
+		self.assertIn('http://localhost:8000/ingredients_detail', self.browser.current_url)
 
 	def test_can_display_ingredients_update(self):
 		self.browser.get('http://localhost:8000/ingredients_update')
 		self.assertIn('Ingredients - Update', self.browser.title)
-		self.assertEqual(
-			'http://localhost:8000/ingredients_update',
-			self.browser.getCurrentUrl()
-		)
+		self.assertIn('http://localhost:8000/ingredients_update', self.browser.current_url)
 
 	def test_can_display_ingredients_create(self):
 		self.browser.get('http://localhost:8000/ingredients_create')
 		self.assertIn('Ingredients - Create', self.browser.title)
-		self.assertEqual(
-			'http://localhost:8000/ingredients_create',
-			self.browser.getCurrentUrl()
-		)
+		self.assertIn('http://localhost:8000/ingredients_create', self.browser.current_url)
 
 	def test_can_display_recipes_list(self):
 		self.browser.get('http://localhost:8000/recipes_list')
 		self.assertIn('Recipes - List', self.browser.title)
-		self.assertEqual(
-			'http://localhost:8000/recipes_list',
-			self.browser.getCurrentUrl()
-		)
+		self.assertIn('http://localhost:8000/recipes_list', self.browser.current_url)
 
 	def test_can_display_recipes_detail(self):
 		self.browser.get('http://localhost:8000/recipes_detail')
 		self.assertIn('Recipes - Detail', self.browser.title)
-		self.assertEqual(
-			'http://localhost:8000/recipes_detail',
-			self.browser.getCurrentUrl()
-		)
+		self.assertIn('http://localhost:8000/recipes_detail', self.browser.current_url)
 
 	def test_can_display_recipes_update(self):
 		self.browser.get('http://localhost:8000/recipes_update')
 		self.assertIn('Recipes - Update', self.browser.title)
-		self.assertEqual(
-			'http://localhost:8000/recipes_update',
-			self.browser.getCurrentUrl()
-		)
+		self.assertIn('http://localhost:8000/recipes_update', self.browser.current_url)
 
 	def test_can_display_recipes_create(self):
 		self.browser.get('http://localhost:8000/recipes_create')
-		self.assertIn('Ingredients - Create', self.browser.title)
-		self.assertEqual(
-			'http://localhost:8000/recipes_create',
-			self.browser.getCurrentUrl()
-		)
+		self.assertIn('Recipes - Create', self.browser.title)
+		self.assertIn('http://localhost:8000/recipes_create', self.browser.current_url)
 
 	def test_can_display_orders_list(self):
 		self.browser.get('http://localhost:8000/orders_list')
 		self.assertIn('Orders - List', self.browser.title)
-		self.assertEqual(
-			'http://localhost:8000/orders_list',
-			self.browser.getCurrentUrl()
-		)
+		self.assertIn('http://localhost:8000/orders_list', self.browser.current_url)
 
 	def test_can_display_orders_detail(self):
 		self.browser.get('http://localhost:8000/orders_detail')
 		self.assertIn('Orders - Detail', self.browser.title)
-		self.assertEqual(
-			'http://localhost:8000/orders_detail',
-			self.browser.getCurrentUrl()
-		)
+		self.assertIn('http://localhost:8000/orders_detail', self.browser.current_url)
 
 	def test_can_display_orders_update(self):
 		self.browser.get('http://localhost:8000/orders_update')
 		self.assertIn('Orders - Update', self.browser.title)
-		self.assertEqual(
-			'http://localhost:8000/orders_update',
-			self.browser.getCurrentUrl()
-		)
+		self.assertIn('http://localhost:8000/orders_update', self.browser.current_url)
 
 	def test_can_display_orders_create(self):
 		self.browser.get('http://localhost:8000/orders_create')
 		self.assertIn('Orders - Create', self.browser.title)
-		self.assertEqual(
-			'http://localhost:8000/orders_create',
-			self.browser.getCurrentUrl()
-		)
-
+		self.assertIn('http://localhost:8000/orders_create', self.browser.current_url)
 
 		self.fail('Finish the test!')
 
